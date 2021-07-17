c78e859207466e4c43d766369d4de5e24c383bf2
Vladimir Lyapin <voviii1984@yandex.ru>
2021-07-17 12:09:41 +0300

    err13

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ const corsOptions = {
       callback(null, false);
     }
   },
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true, // Credentials are cookies, authorization headers or TLS client certificates
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept'],
